87cbe0e07d70598eba81a7fc52c471bdbbf0d653
Omri <516342+omrilotan@users.noreply.github.com>
2018-11-06 17:52:44 +0200

    Use async tests for fetch (#18)

--- src/tests/fetch.test.js
+++ src/tests/fetch.test.js
@@ -18,15 +18,10 @@ describe('Gofor', () => {
             );
         });
 
-        it('Shoots a fetch request', (done) => {
-            gofor.fetch('https://www.fiverr.com')
-                .then((res) => {
-                    assert.include(res.url, 'https://www.fiverr.com');
-                    done();
-                }).catch((error) => {
-                    catcher(error);
-                    done();
-                });
+        it('Shoots a fetch request', async() => {
+            const res = await gofor.fetch('http://httpbin.org/get?param=value');
+
+            assert.equal((await res.json()).args.param, 'value');
         });
     });
 });
